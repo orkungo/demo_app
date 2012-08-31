c6ce3c9848c714be0c566c871c35e70dc3d3a608
orkungo <orkungo@yahoo.com>
2012-08-31 11:06:41 +0200

    Users created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '43aeb42544e750eefa8242bb0a516725f7357dba1ee36a6c77a2048780807ddbbc9a93290ee58bffe4ab5272325479983b8876aa34edee7184014940d1bbdbab'
